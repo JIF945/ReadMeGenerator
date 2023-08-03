b8d337946d13ef13541be93be69a9990551160bb
James <joshua.james918@gmail.com>
2023-08-02 22:59:24 -0500

    new questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,102 +1,70 @@
 // TODO: Include packages needed for this application
 const fs = require("fs")
 const inquirer = require('inquirer')
+const generateMarkDown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
-        message: 'What is your Motivation?',
-        name: 'motivation'
+        name: 'title',
+        message: 'what is the title of your project'
+
     },
     {
         type: 'input',
-        message: 'why did you build this project',
-        name: 'the why'
+        name: 'motivation',
+        message: 'What is your Motivation?'
+        
     },
     {
         type: 'input',
+        name: 'the why',
+        message: 'why did you build this project'
+    },
+    {
+        type: 'input',
+        name: 'solution',
         message: 'what problem does it solve?',
-        name: 'what'
+        
     },
     {
         type: 'input',
-        message: 'what did you learn',
-        name: 'learned'
+        name: 'skills',
+        message: 'what did you learn'
     },
     {
         type:'input',
-        message: 'what makes your project stand out',
-        name: 'stand out'
-        
+        name: 'special',
+        message: 'what makes your project stand out',   
+    },
+    {
+        type: 'editor',
+        name: 'installation',
+        message:'What are the steps required to install your project?'
+    },
+    {
+        type: 'editor',
+        name: 'usage',
+        message:' provide instructions and examples for use'
+    },
+    {
+        type: 'editor',
+        name: 'Credits',
+        message: " list,  collaborators, third party assets or tutorials used to to make this project. " 
+    },
+    {
+        type: 'list',
+        name: 'License',
+        message: 'List any license used for the project if your unsure refer to [https://choosealicense.com/](https://choosealicense.com/)'
     }
+
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(generateMarkDown, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
-
-const fs = require('fs');
-const inquirer = require('inquirer');
-const { type } = require("os");
-
-// Questions for user
-// name, location, bio, LinkedIn URL, and GitHub URL
-// const questions = [
-//     {
-//         type: 'input',
-//         message: 'What is your name?',
-//         name: 'name'
-//     },
-//     {
-//         type: 'input',
-//         message: 'What is your location?',
-//         name: 'location'
-//     },
-//     {
-//         type: 'input',
-//         message: 'Give us a brief bio:',
-//         name: 'bio'
-//     },
-// ];
-
-// // Prompt user with questions
-// inquirer
-//     .prompt(questions)
-//     .then((answers) => {
-//         // Use answers to write HTML
-//         const html = getHtml(answers);
-
-//         fs.writeFile('bio.html', html, (err) => {
-//             if (err) {
-//                 console.error(err);
-//             } else {
-//                 console.log("Write file success!");
-//             }
-//         });
-//     });
-
-// const getHtml = (answers) => {
-//     const { name, location, bio } = answers;
-
-//     return `
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <!-- ADD BOOTSTRAP HERE -->
-//     <title>Developer Bio</title>
-// </head>
-// <body>
-//     <h1>${name}</h1>
-//     <p>${location}</p>
-//     <p>${bio}</p>
-// </body>
-// </html>
-// `;
-// }
