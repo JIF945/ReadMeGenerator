63a19a4916bf18507f29f11207e3d37c86300f74
James <joshua.james918@gmail.com>
2023-08-03 21:24:16 -0500

    function i guess

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,9 @@
 // TODO: Include packages needed for this application
-const fs = require("fs")
+
+const fs = require("fs");
 const inquirer = require('inquirer');
 const generateMarkdown = require("./utils/generateMarkdown");
+
 // TODO: Create an array of questions for user input
 
 const questions = [
@@ -64,9 +66,7 @@ const questions = [
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    
-}
+function writeToFile(README, questions) {}
 
 // TODO: Create a function to initialize app
 function init() {}
